c498956f0da5a0745f65d56459cc5f7ed69355d2
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2021-06-30 19:24:48 -0300

    Less cheating

--- main.js
+++ main.js
@@ -1,36 +1,69 @@
-winnerHTML = document.getElementById("winner")
-bangHTML = document.getElementById("bang")
+scoreboardHTML = document.getElementById("scoreboard");
+bangHTML = document.getElementById("bang");
+losers = [];
 
 function clean() {
-    winnerHTML.innerHTML = ''
-    bangHTML.innerHTML = ''
+  losers = [];
+  scoreboardHTML.innerHTML = "";
+  bangHTML.innerHTML = "";
 }
 
-function showWinner(winner) {
-    winnerHTML.innerHTML = winner + ' wins'
+function showScoreboard(winner) {
+  let scoreboardText = losers
+    .map((loser) => loser + " shoot before the BANG<br>")
+    .join("");
+  scoreboardText += winner + " wins<br>";
+  scoreboardHTML.innerHTML = scoreboardText;
+}
+
+function handleLosers(e) {
+  losers.push(e.key);
 }
 
 function waitForWinner() {
-    document.addEventListener("keydown", e => {
-        showWinner(e.key)
-        }, {once: true});
+  bangHTML.innerHTML = "BANG";
+  document.removeEventListener("keydown", handleLosers);
+
+  waitForWinnerEvadingLosers();
+}
+
+function waitForWinnerEvadingLosers() {
+  document.addEventListener(
+    "keydown",
+    (e) => {
+      if (losers.includes(e.key)) {
+        console.log(e.key + ', no da bro ya perdiste')
+        waitForWinnerEvadingLosers();
+        return;
+      }
+      showScoreboard(e.key);
+      pressSpaceToStartGame();
+    },
+    { once: true }
+  );
 }
 
 function showdown(startsIn, timeRange) {
-    // Convert to miliseconds
-    startsIn *= 1000
-    timeRange *= 1000
+  // Convert to miliseconds
+  startsIn *= 1000;
+  timeRange *= 1000;
 
-    timeToWait = startsIn + Math.random() * timeRange
-    setTimeout(() => {
-        bangHTML.innerHTML = "BANG"
-        waitForWinner()
-    }, timeToWait);
+  timeToWait = startsIn + Math.random() * timeRange;
+  document.addEventListener("keydown", handleLosers);
+  setTimeout(waitForWinner, timeToWait);
 }
 
-document.addEventListener("keydown", e => {
-    if (e.key == " ") {
-        clean()
-        showdown(3, 4)
-    }
-    });
+function pressSpaceToStartGame() {
+  document.addEventListener(
+    "keydown",
+    (e) => {
+      if (e.key == " ") {
+        clean();
+        showdown(3, 4);
+      }
+    },
+    { once: true }
+  );
+}
+
+pressSpaceToStartGame();

74b4a21bef3435925adafe00444229a649c64763
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2021-07-01 15:19:54 -0300

    More progress, but we are deprecating the branch

--- main.js
+++ main.js
@@ -39,106 +39,198 @@
 const ShowDown = {
   data() {
     return {
-      message: "xd",
+      losers: [],
+      timeToWait: 0,
+      bangTime: 0,
+      startGameKey: " ",
+      bangText: "BANG",
+      scoreboardHTML: "",
+      bangHTML: "",
     };
+  },
+  methods: {
+    clean() {
+      this.losers = [];
+      this.scoreboardHTML = "";
+      this.bangHTML = "";
+    },
+    showScoreboard(winner) {
+      let scoreboardText = this.losers
+        .map(
+          (loser) =>
+            `${loser.key.fontcolor("red")} shoot ${(loser.delta / 1000).toFixed(
+              3
+            )} seconds before the ${bangText} and lost!<br>`
+        )
+        .join("");
+
+      winnerDelaySeconds = (nowTime() - bangTime) / 1000;
+      scoreboardText += `<br> ${winner.fontcolor(
+        "green"
+      )} is the last one standing by shooting ${winnerDelaySeconds.toFixed(
+        3
+      )} after ${bangText}<br>Good reflexes kiddo<br>The waiting time was ${(
+        timeToWait / 1000
+      ).toFixed(3)} seconds`;
+      this.scoreboardHTML = scoreboardText;
+    },
+    nowTime() {
+      return new Date().getTime();
+    },
+
+    handleLosers(e) {
+      if (this.losers.map((loser) => loser.key).includes(e.key)) return;
+      let delta = this.bangTime - nowTime();
+      this.losers.push({ key: e.key, delta: delta });
+    },
+
+    waitForWinner() {
+      bangHTML = bangText;
+      document.removeEventListener("keydown", handleLosers);
+
+      waitForWinnerEvadingLosers();
+    },
+
+    waitForWinnerEvadingLosers() {
+      document.addEventListener(
+        "keydown",
+        (e) => {
+          if (this.losers.map((loser) => loser.key).includes(e.key)) {
+            waitForWinnerEvadingLosers();
+            return;
+          }
+          showScoreboard(e.key);
+          pressKeyToStartGame();
+        },
+        { once: true }
+      );
+    },
+
+    randomBetween(lowBound, highBound) {
+      return Math.random() * (highBound - lowBound) + lowBound;
+    },
+
+    showdown(fromTime, toTime) {
+      // Convert to miliseconds
+      timeToWait = randomBetween(fromTime, toTime) * 1000;
+
+      bangTime = nowTime() + timeToWait;
+      document.addEventListener("keydown", handleLosers);
+      setTimeout(waitForWinner, timeToWait);
+    },
+
+    pressKeyToStartGame() {
+      document.addEventListener(
+        "keydown",
+        (e) => {
+          if (e.key != startGameKey) {
+            pressKeyToStartGame();
+            return;
+          }
+          clean();
+          showdown(3, 4);
+        },
+        { once: true }
+      );
+    },
   },
 };
 Vue.createApp(ShowDown).mount("#ShowDown");
 
-scoreboardHTML = document.getElementById("scoreboard");
-bangHTML = document.getElementById("bang");
-rules = document.getElementById("rules");
-losers = [];
-timeToWait = 0;
-bangTime = 0;
-startGameKey = " ";
-bangText = "BANG";
+// scoreboardHTML = document.getElementById("scoreboard");
+// bangHTML = document.getElementById("bang");
+// rules = document.getElementById("rules");
+// losers = [];
+// timeToWait = 0;
+// bangTime = 0;
+// startGameKey = " ";
+// bangText = "BANG";
 
-rules.innerHTML = "Press " + "spacebar" + " to begin the showdown";
+// rules.innerHTML = "Press " + "spacebar" + " to begin the showdown";
 
-function clean() {
-  losers = [];
-  scoreboardHTML.innerHTML = "";
-  bangHTML.innerHTML = "";
-}
+// function clean() {
+//   losers = [];
+//   scoreboardHTML.innerHTML = "";
+//   bangHTML.innerHTML = "";
+// }
 
-function showScoreboard(winner) {
-  let scoreboardText = losers
-    .map(
-      (loser) =>
-        `${loser.key.fontcolor("red")} shoot ${(loser.delta / 1000).toFixed(
-          3
-        )} seconds before the ${bangText} and lost!<br>`
-    )
-    .join("");
+// function showScoreboard(winner) {
+//   let scoreboardText = losers
+//     .map(
+//       (loser) =>
+//         `${loser.key.fontcolor("red")} shoot ${(loser.delta / 1000).toFixed(
+//           3
+//         )} seconds before the ${bangText} and lost!<br>`
+//     )
+//     .join("");
 
-  winnerDelaySeconds = (nowTime() - bangTime) / 1000;
-  scoreboardText += `<br> ${winner.fontcolor(
-    "green"
-  )} is the last one standing by shooting ${winnerDelaySeconds.toFixed(
-    3
-  )} after ${bangText}<br>Good reflexes kiddo<br>The waiting time was ${(
-    timeToWait / 1000
-  ).toFixed(3)} seconds`;
-  scoreboardHTML.innerHTML = scoreboardText;
-}
+//   winnerDelaySeconds = (nowTime() - bangTime) / 1000;
+//   scoreboardText += `<br> ${winner.fontcolor(
+//     "green"
+//   )} is the last one standing by shooting ${winnerDelaySeconds.toFixed(
+//     3
+//   )} after ${bangText}<br>Good reflexes kiddo<br>The waiting time was ${(
+//     timeToWait / 1000
+//   ).toFixed(3)} seconds`;
+//   scoreboardHTML.innerHTML = scoreboardText;
+// }
 
-nowTime = () => new Date().getTime();
+// nowTime = () => new Date().getTime();
 
-function handleLosers(e) {
-  if (losers.map((loser) => loser.key).includes(e.key)) return;
-  let delta = bangTime - nowTime();
-  losers.push({ key: e.key, delta: delta });
-}
+// function handleLosers(e) {
+//   if (losers.map((loser) => loser.key).includes(e.key)) return;
+//   let delta = bangTime - nowTime();
+//   losers.push({ key: e.key, delta: delta });
+// }
 
-function waitForWinner() {
-  bangHTML.innerHTML = "BANG";
-  document.removeEventListener("keydown", handleLosers);
+// function waitForWinner() {
+//   bangHTML.innerHTML = "BANG";
+//   document.removeEventListener("keydown", handleLosers);
 
-  waitForWinnerEvadingLosers();
-}
+//   waitForWinnerEvadingLosers();
+// }
 
-function waitForWinnerEvadingLosers() {
-  document.addEventListener(
-    "keydown",
-    (e) => {
-      if (losers.map((loser) => loser.key).includes(e.key)) {
-        waitForWinnerEvadingLosers();
-        return;
-      }
-      showScoreboard(e.key);
-      pressKeyToStartGame();
-    },
-    { once: true }
-  );
-}
+// function waitForWinnerEvadingLosers() {
+//   document.addEventListener(
+//     "keydown",
+//     (e) => {
+//       if (losers.map((loser) => loser.key).includes(e.key)) {
+//         waitForWinnerEvadingLosers();
+//         return;
+//       }
+//       showScoreboard(e.key);
+//       pressKeyToStartGame();
+//     },
+//     { once: true }
+//   );
+// }
 
-function randomBetween(lowBound, highBound) {
-  return Math.random() * (highBound - lowBound) + lowBound;
-}
+// function randomBetween(lowBound, highBound) {
+//   return Math.random() * (highBound - lowBound) + lowBound;
+// }
 
-function showdown(fromTime, toTime) {
-  // Convert to miliseconds
-  timeToWait = randomBetween(fromTime, toTime) * 1000;
+// function showdown(fromTime, toTime) {
+//   // Convert to miliseconds
+//   timeToWait = randomBetween(fromTime, toTime) * 1000;
 
-  bangTime = nowTime() + timeToWait;
-  document.addEventListener("keydown", handleLosers);
-  setTimeout(waitForWinner, timeToWait);
-}
+//   bangTime = nowTime() + timeToWait;
+//   document.addEventListener("keydown", handleLosers);
+//   setTimeout(waitForWinner, timeToWait);
+// }
 
-function pressKeyToStartGame() {
-  document.addEventListener(
-    "keydown",
-    (e) => {
-      if (e.key != startGameKey) {
-        pressKeyToStartGame();
-        return;
-      }
-      clean();
-      showdown(3, 4);
-    },
-    { once: true }
-  );
-}
+// function pressKeyToStartGame() {
+//   document.addEventListener(
+//     "keydown",
+//     (e) => {
+//       if (e.key != startGameKey) {
+//         pressKeyToStartGame();
+//         return;
+//       }
+//       clean();
+//       showdown(3, 4);
+//     },
+//     { once: true }
+//   );
+// }
 
-pressKeyToStartGame();
+// pressKeyToStartGame();

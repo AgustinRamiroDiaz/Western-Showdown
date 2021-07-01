a98e0c58a2541b969edfa5bccdcab5d86c2f960a
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2021-07-01 15:57:10 -0300

    Late players included

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 scoreboardHTML = document.getElementById("scoreboard");
 bangHTML = document.getElementById("bang");
 rules = document.getElementById("rules");
-losers = [];
-timeToWait = 0
+earlyPlayers = [];
+latePlayers = [];
+timeToWait = 0;
 bangTime = 0;
 startGameKey = " ";
+winner = "";
 
 rules.innerHTML = "Press " + "spacebar" + " to begin the showdown";
 
 function clean() {
-  losers = [];
+  earlyPlayers = [];
   scoreboardHTML.innerHTML = "";
   bangHTML.innerHTML = "";
+  rules.innerHTML = "";
 }
 
-function showScoreboard(winner) {
-  let scoreboardText = losers
+function showScoreboard() {
+  let scoreboardText = earlyPlayers
     .map(
-      (loser) =>
-        loser.key.fontcolor("red") +
+      (earlyPlayer) =>
+        earlyPlayer.key.fontcolor("red") +
         " shoot " +
-        (loser.delta / 1000).toFixed(3) +
+        (earlyPlayer.delta / 1000).toFixed(3) +
         " seconds before the BANG and lost!<br>"
     )
     .join("");
@@ -31,16 +34,28 @@ function showScoreboard(winner) {
     winner.fontcolor("green") +
     " is the last one standing by shooting " +
     winnerDelaySeconds.toFixed(3) +
-    " after BANG<br>Good reflexes kiddo<br>The waiting time was " + (timeToWait/1000).toFixed(3) + " seconds";
+    " after BANG<br>Good reflexes kiddo<br>The waiting time was " +
+    (timeToWait / 1000).toFixed(3) +
+    " seconds<br>";
+
+  scoreboardText += latePlayers
+    .map(
+      (latePlayer) =>
+        `${latePlayer.key.fontcolor("red")} is late by ${(
+          latePlayer.delta / 1000
+        ).toFixed(3)}`
+    )
+    .join("");
   scoreboardHTML.innerHTML = scoreboardText;
 }
 
 nowTime = () => new Date().getTime();
 
 function handleLosers(e) {
-  if (losers.map((loser) => loser.key).includes(e.key)) return
+  if (earlyPlayers.map((earlyPlayer) => earlyPlayer.key).includes(e.key))
+    return;
   let delta = bangTime - nowTime();
-  losers.push({ key: e.key, delta: delta });
+  earlyPlayers.push({ key: e.key, delta: delta });
 }
 
 function waitForWinner() {
@@ -50,17 +65,33 @@ function waitForWinner() {
   waitForWinnerEvadingLosers();
 }
 
+function catchLatePlayers(e) {
+  console.log("I catched " + e.key + " too late");
+  // TODO: restrict also earlyPlayers
+  if (latePlayers.map((latePlayer) => latePlayer.key).includes(e.key)) return;
+  let delta = nowTime() - bangTime;
+  latePlayers.push({ key: e.key, delta: delta });
+  showScoreboard();
+}
+
 function waitForWinnerEvadingLosers() {
   document.addEventListener(
     "keydown",
     (e) => {
-      if (losers.map((loser) => loser.key).includes(e.key)) {
+      if (earlyPlayers.map((earlyPlayer) => earlyPlayer.key).includes(e.key)) {
         console.log(e.key + ", no da bro ya perdiste");
         waitForWinnerEvadingLosers();
         return;
       }
-      showScoreboard(e.key);
-      pressKeyToStartGame();
+      winner = e.key;
+      showScoreboard();
+
+      document.addEventListener("keydown", catchLatePlayers);
+      setTimeout(() => {
+        document.removeEventListener("keydown", catchLatePlayers);
+        pressKeyToStartGame();
+        rules.innerHTML = "Press " + "spacebar" + " to begin the showdown";
+      }, 1000);
     },
     { once: true }
   );
